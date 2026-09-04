c5b21b0645ea4715b062ba700e7fbf2f5fe34f27
amritdubey78999-hub <amritdubey78999@gmail.com>
2026-09-04 22:50:32 +0530

    Create LogicalOperator.java

---
+++ LogicalOperator.java
@@ -0,0 +1,16 @@
+public class LogicaOperator {
+    public static void main(String[] args) {
+        
+    
+    // Logical Operators
+    int a = 5;
+    int b = 10;
+    int c = 15;
+    /*  boolean d = (a<b)&&(b < c);
+    System.out.println(d);//true */
+    boolean d = (a < b)||(b < c) ;
+    System.out.println(d);//true
+    }
+}
+//public class main(){
+//public static void main(string[] args)
