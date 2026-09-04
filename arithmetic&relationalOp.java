bc3e2867c8c309a0b4e45983aa8684b1bae7bf45
amritdubey78999-hub <amritdubey78999@gmail.com>
2026-09-04 22:51:25 +0530

    Create arithmetic&relationalOp.java

---
+++ arithmetic&relationalOp.java
@@ -0,0 +1,57 @@
+public class operatorss {
+    public static void main(String[] args) {
+        //Operators in java
+        //Arithmetic operators -->+ - * % / += -= /= %= ++ --
+        /* 
+         int a =5;
+         int b =10;
+        int h = a + 2;//..output(5+2=7) aayega.
+        int h = h + 2;//..output(h=7 isliye 7+2=9 print hoga)  
+         //..(h+=2)isko aise bhi likh skte hai 
+         h += 2; //h = h+2;//h=9
+         h -= 2; //h = h-2;//h=7
+         h *= 2; //h = h*2;
+         h /= 2; //h = h/2;
+         h %= 2; //h = h%2;
+
+         int i = 6;
+         i++; //i = i + 1 -->i +=1; //output7 aayega 
+         i--; //i = i - 1 -->i -+1; //output 5 aayega 
+
+         //pre increment and post incrememnt
+         int j = 7;
+         j++; // postfix increment
+         ++j; //prefix increment
+        
+        int k = j++; // k = j; j=j+1;
+        System.out.println(j +" ," +k); // 10,9
+         */
+
+       //Relational OPerator : == != < > <= >=
+       /* 
+       int a=5;
+       int b=10;
+       a==b; //Is a is equal to b ?
+       a=b; // assign value of b to a. assignmnet operator
+       boolean c = (a==b);
+       System.out.println(c);
+
+       a!=b;//Is a is not equal to b ?
+       c =(a!=b);
+       boolean c =(a<b); //is a<b?
+       c=(a>b);
+       c = (a<=b);//c=(a < or = to b);
+
+        int a = 10;
+        int b = 10;
+        boolean c = (a == b);//true
+      
+        boolean d = (a != b);//false
+        
+        boolean e = (a < b); //false
+
+        boolean f = (a <= b); //true
+        */
+
+    }
+}
