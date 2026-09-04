63870771ce4c4b488f1f6b5487d9dd6eba3ae216
amritdubey78999-hub <amritdubey78999@gmail.com>
2026-09-04 22:49:39 +0530

    Create bitwiseandshiftoperator.java

---
+++ bitwiseandshiftoperator.java
@@ -0,0 +1,25 @@
+public class bitwiseoperator {
+    public static void main(String[] args) {
+     /*   //BITWISE OPERATOR -- bit manupaltion
+        // &  |  ^  ~  >>  <<  >>>  &=  |=  ^=  >>=  <<=  >>>=
+        int a=2;//2 --> 1 0 -->00000000 00000000 00000000 00000010
+        int b=3;//3 --> 1 1 -->00000000 00000000 00000000 00000011
+        int c = a & b;// --> 1 0 -->2
+        int d = a | b;//1 1 --> 3
+        int e = a ^ b;//0 1 -->2
+        int f = ~a;// 11111111 11111111 11111111 11111101 →—> -3
+        System.out.println(c +" , " + d + " , " + e +" , "+ f);
+    */
+      /* 
+        // Shift operations
+        int g = 1; //00000000 00000000 00000000 00000001--> 1
+        g = g <<31;//-->32%32=0==g=g<<0== 1 //000000000 00000000 00000000 000000000 -->
+    
+        // Right shift (>>) (>>>)
+        byte h = 1; // 00000001
+        h = (byte) (h << 1); // 00000000 00000000 00000000 00000010
+        System.out.println(g);
+        Svstem.out.println(h);
+        */
+    }
+}
